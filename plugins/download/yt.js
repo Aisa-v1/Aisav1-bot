0d87ed52c85c37c14353264798291d34aa42bb65
Aisa-v1 <asyaq80@gmail.com>
2026-06-10 05:57:01 +0300

    Update yt.js

--- plugins/download/yt.js
+++ plugins/download/yt.js
@@ -28,8 +28,8 @@ const handler = async (m, { conn, command, text }) => {
       isForwarded: true,
       forwardingScore: 1,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '120363426553571462@newsletter',
+        newsletterName: 'Aisa-v1',
         serverMessageId: 0
       },
       externalAdReply: {
