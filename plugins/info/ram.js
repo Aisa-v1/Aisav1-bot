a500e91c7547b2a1f9432266af56389d5248e903
Aisa-v1 <asyaq80@gmail.com>
2026-06-10 05:47:53 +0300

    Update ram.js

--- plugins/info/ram.js
+++ plugins/info/ram.js
@@ -9,13 +9,13 @@ const handler = async (m, { conn }) => {
 `;
 
   await conn.msgUrl(m.chat, txt, {
-    img: "https://i.pinimg.com/736x/73/56/32/735632c6fa8e665c249abbc8a340b77d.jpg" ,
+    img: "https://litter.catbox.moe/c6ikee.jpg" ,
     caption: txt,
     title: "𝐒𝐩𝐞𝐞𝐝 / 𝐓𝐞𝐬𝐭",
     body: "𝐓𝐞𝐬𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐛𝐨𝐭'𝐬 𝐬𝐩𝐞𝐞𝐝: 𝐈𝐬 𝐢𝐭 𝐟𝐚𝐬𝐭 𝐨𝐫 𝐧𝐨𝐭?",
     newsletter: {
-      name: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
-      jid: '120363225356834044@newsletter'
+      name: 'Aisa-v1',
+      jid: '120363426553571462@newsletter'
     },
     big: false,
     mentions: [m.sender]
